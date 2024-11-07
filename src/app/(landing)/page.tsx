0d4787cdfb152c6a41874cbf33cb3044bfbeea51
growwidtalha <growwithtalha2@gmail.com>
2024-11-07 20:50:59 +0500

    bug: bento grid fix

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -11,7 +11,7 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism"
 
 const Page = () => {
-  const codeSnippet = `await fetch("http://localhost:3000/api/v1/events", {
+  const codeSnippet = `await fetch("${process.env.NEXT_PUBLIC_APP_URL}/api/v1/event", {
   method: "POST",
   body: JSON.stringify({
     category: "sale",
